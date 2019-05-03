ae6517aecbdcff66df41e09749ea7e25e8516b33
User <artyom.hovakimyan01@gmail.com>
2019-05-03 16:30:45 +0400

    add categories

---
+++ Mic.Volo.PFMCoreApp/PFMDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mic.Volo.PFMCoreApp
+{
+    public class PFMDbContext : DbContext
+    {
+        public DbSet<Wallet> Wallets { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=PFM_EFCore;Integrated Security=True");
+        }
+    }
+
+    public class Wallet
+    {
+        public int WalletId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public string Comment { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Wallet> Wallets { get; set; }
+    }
+}
---
+++ Mic.Volo.PFMCoreApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Mic.Volo.PFMCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (PFMDbContext db = new PFMDbContext())
+            {
+                Category[] categories = new Category[10];
+                for (int i = 0; i < categories.Length; i++)
+                {
+                    categories[i] = new Category
+                    {
+                        
+                        Title = "Title" + i.ToString(),
+                        Description = "Desc" + i.ToString(),
+
+                    };
+                    db.Categories.Add(categories[i]);
+                }
+                db.SaveChanges();
+            }
+
+            //using (var db = new PFMDbContext())
+            //{
+            //    var categories = new Category[]
+            //    {
+            //        new Category
+            //        {
+            //            Title = "Cat 1"
+            //        },
+            //        new Category
+            //        {
+            //            Title = "Cat 2",
+            //            Description = "Dummy desc"
+            //        }
+            //    };
+
+            //    db.Categories.AddRange(categories);
+
+            //    db.SaveChanges();
+
+            //    var cat1 = db.Categories.FirstOrDefault(cat => cat.Title == "Cat 1");
+
+            //    db.Wallets.Add(new Wallet
+            //    {
+            //        Amount = 50,
+            //        Category = cat1,
+            //        Date = DateTime.UtcNow
+            //    });
+
+            //    db.SaveChanges();
+
+            //    var wallet1 = db.Wallets.Include(wallet => wallet.Category).First();
+
+            //    var category = db.Categories.Include(cat => cat.Wallets).First();
+
+            //    var target = db.Wallets.SingleOrDefault(p => p.WalletId == 1);
+
+            //    if (target != null)
+            //    {
+            //        db.Wallets.Remove(target);
+            //        db.SaveChanges();
+            //    }
+
+        }
+    }
+}
+
